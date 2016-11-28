ca1d1d1847630cd190b19645d4fcc995a72c30ae
Daniel Gibson <soulninetynine@gmail.com>
2016-11-28 14:09:55 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang = "en" class="background">
+  <head>
+    <!-- basic.html -->
+    <title>DING</title>
+    <meta charset = "UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
+  	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+
+
+      <script   src="https://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
+      <script type="text/javascript" src="script.js"></script>
+    </body>
+
+</html>
